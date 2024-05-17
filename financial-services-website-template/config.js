92aa3342d8ba162c7a879d7bf630040308166885
0nlyAriel <arielf9shop@gmail.com>
2024-05-17 13:13:59 +0100

    Nearly Done

---
+++ financial-services-website-template/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkpVR-SSGm37FTLl89Hbgq7yozc1UaLOs",
+    authDomain: "finanza-investment-site.firebaseapp.com",
+    projectId: "finanza-investment-site",
+    storageBucket: "finanza-investment-site.appspot.com",
+    messagingSenderId: "914137236217",
+    appId: "1:914137236217:web:d271d64034fa60de97d828",
+    measurementId: "G-B826SQ38PQ"
+};
+export {firebaseConfig};
